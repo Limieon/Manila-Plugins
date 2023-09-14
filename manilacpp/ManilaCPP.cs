f9bc9f933c743a3f9ffb60352e26aeca76c3cf80
GMasterHD <xxgmasterhdxx@gmail.com>
2023-09-14 09:34:59 +0200

    Added Plugin ManilaCPP

---
+++ manilacpp/ManilaCPP.cs
@@ -0,0 +1,16 @@
+﻿
+using Manila.Plugin.API;
+
+namespace ManilaCPP;
+
+public class ManilaCPP: Plugin {
+	public ManilaCPP(): base("manilacpp") {
+	}
+
+	public override void init() {
+		print("Initializing...");
+	}
+	public override void shutdown() {
+		print("Shutting down...");
+	}
+}
