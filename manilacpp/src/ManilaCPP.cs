27c117e87f3cd1921fa0e39a4a5fc7bb2fb3ed0c
GMasterHD <xxgmasterhdxx@gmail.com>
2023-09-15 20:14:21 +0200

    Added Script Function Tests

--- manilacpp/src/ManilaCPP.cs
+++ manilacpp/src/ManilaCPP.cs
@@ -1,16 +1,23 @@
 ﻿
+using Manila;
 using Manila.Plugin.API;
+
+using ManilaCPP.Clang;
 
 namespace ManilaCPP;
 
 public class ManilaCPP: Plugin {
+	public static ManilaCPP instance { get; private set; }
+
 	public ManilaCPP(): base("manilacpp") {
+		instance = this;
 	}
 
 	public override void init() {
-		print("Initializing...");
+		debug("Initializing...");
+		addType(typeof(Clang.Clang));
 	}
 	public override void shutdown() {
-		print("Shutting down...");
+		debug("Shutting down...");
 	}
 }
---
+++ manilacpp/src/clang/Clang.cs
@@ -0,0 +1,16 @@
+
+using Manila.Plugin.API;
+
+namespace ManilaCPP.Clang;
+
+public class Flags {
+	public string name { get; set; }
+}
+
+public static class Clang {
+	public static Flags flags() { return new Flags(); }
+
+	public static void compile(Flags flags) {
+		ManilaCPP.instance.info("Compiling", flags.name + "...");
+	}
+}
