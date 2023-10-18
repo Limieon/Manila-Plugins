94b82a391c8819315c11b2872fd505837f901c46
GMasterHD <justin.jakob391@gmail.com>
2023-10-18 19:29:49 +0200

    Added solution file generation

--- manilacpp/src/api/MSBuild.cs
+++ manilacpp/src/api/MSBuild.cs
@@ -8,7 +8,6 @@ namespace ManilaCPP.API;
 
 public static class MSBuild {
 	private static ManilaCPP plugin = ManilaCPP.instance;
-
 	public static List<ProjectFile> files = new List<ProjectFile>();
 
 	public class Flags {
@@ -37,7 +36,7 @@ public static class MSBuild {
 	public static Flags.BinaryType staticLib() { return Flags.BinaryType.STATIC_LIB; }
 	public static Flags.BinaryType dynamicLib() { return Flags.BinaryType.DYNAMIC_LIB; }
 
-	public static ManilaFile build(Workspace workspace, Project project, BuildConfig config, Flags flags) {
+	public static void project(Workspace workspace, Project project, BuildConfig config, Flags flags) {
 		var prj = new ProjectFile(project, flags.objDir, flags.binDir);
 
 		prj.srcFiles.AddRange(flags.srcFiles);
@@ -64,20 +63,9 @@ public static class MSBuild {
 
 		prj.generate((CPPBuildConfig) config, flags);
 
-		var dir = Directory.GetCurrentDirectory();
-		Directory.SetCurrentDirectory(project.location.getPath());
-
-		var i = new ProcessStartInfo("msbuild.exe");
-		i.Arguments = Compiler.arguments((CPPBuildConfig) config, flags);
-
-		var p = Process.Start(i);
-		p.WaitForExit();
-
-		ManilaCPP.instance.debug("Exiting Dir...");
-		Directory.SetCurrentDirectory(dir);
-
 		files.Add(prj);
-
-		return new ManilaFile(flags.binDir.getPath(), $"{project.name}.exe");
+	}
+	public static void workspace(Workspace workspace) {
+		SolutionFile.generate(workspace, new ManilaFile(workspace.location, $"{workspace.name}.sln"));
 	}
 }
