c59fab4e1905ddb14f8c5447b3518f125342d47c
Limieon <xxgmasterhdxx@gmail.com>
2023-10-31 04:50:05 +0100

    Improved fileSet API

--- manilacpp/src/templates/ApplicationProjectConfigurator.cs
+++ manilacpp/src/templates/ApplicationProjectConfigurator.cs
@@ -1,30 +1,29 @@
 
 using Manila.Plugin.API;
 using Manila.Scripting.API;
+using Manila.Utils;
 using Microsoft.ClearScript;
 
 public class ApplicationProjectConfigurator : ProjectConfigurator {
 	public ApplicationProjectConfigurator() { }
 
 	public override void init() {
-		_fileSets = new Dictionary<string, FileSet>();
+		_fileSets = new Dictionary<string, ManilaFile[]>();
 	}
 
-	internal Dictionary<string, FileSet> _fileSets;
+	internal Dictionary<string, ManilaFile[]> _fileSets;
 
-	public ApplicationProjectConfigurator fileSets(ManilaDirectory root, ScriptObject obj) {
+	public ApplicationProjectConfigurator fileSets(ScriptObject obj) {
 		foreach (var k in obj.PropertyNames) {
-			var fs = new FileSet(root.getPath());
-			((ScriptObject) obj[k]).InvokeAsFunction(fs);
-			_fileSets.Add(k, fs);
+			_fileSets.Add(k, (ManilaFile[]) obj[k]);
 		}
 		return this;
 	}
 
 	public override Dictionary<string, dynamic> getProperties() {
 		foreach (var e in _fileSets) {
-			ManilaCPP.ManilaCPP.instance.info($"Name: '{e.Key}', Root: {e.Value.root}");
-			foreach (var f in e.Value.files()) {
+			ManilaCPP.ManilaCPP.instance.info($"Name: '{e.Key}'");
+			foreach (var f in e.Value) {
 				ManilaCPP.ManilaCPP.instance.info($"\t{f}");
 			}
 		}
